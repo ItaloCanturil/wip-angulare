403b80c5252f884c877a76b85500401b7d45b7fa
Italo Canturil <italo.cantur@gmail.com>
2022-07-31 18:39:11 -0300

    init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Repository from '@/plugins/Repository'
 
 Vue.config.productionTip = false
+
+Vue.use(Repository)
 
 new Vue({
   router,
