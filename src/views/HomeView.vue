47b5b75ae7c0188a0e9c0b41813169e12e908592
Italo Canturil <italo.cantur@gmail.com>
2022-08-01 22:33:47 -0300

    feat: Add post components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
+  <div class="home bg-[#16161a] relative flex flex-col items-center min-h-screen justify-center">
     <Header/>
-    <PostComponent/>
+    <PostComponent :data="posts"/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
 import PostComponent from '@/components/PostsComponent.vue'
+import { mapState, mapActions } from 'vuex'
 export default {
   name: 'HomeView',
   components: {
     Header,
     PostComponent
+  },
+  computed: {
+    ...mapState({
+      posts: state => state.posts.data
+    })
+  },
+  async created () {
+    await this.getAll()
+  },
+  methods: {
+    ...mapActions({
+      getAll: 'posts/getAllPosts'
+    })
   }
 }
 </script>
