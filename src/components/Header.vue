a4f96c3ac70e922d34c66c10b64daa4004bc9e35
Italo Canturil <italo.cantur@gmail.com>
2022-08-01 22:31:34 -0300

    feat: Create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header w-full fixed top-0 bg-white flex justify-between px-3 h-14 items-center">
+    <h1>Wip Angulare</h1>
+
+    <button class="bg-[#7f5af0] text-[#fffffe] py-1 px-2 rounded-lg" @click="$emit('showUser')">
+      Adicionar usuário
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+
+}
+</script>
