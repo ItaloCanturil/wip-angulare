fc498c0564e7750aeca6d4329c476541e5ea1f89
Italo Canturil <italo.cantur@gmail.com>
2022-08-01 22:33:32 -0300

    feat: Create PostModal

---
+++ src/components/Modal/PostModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="
+      fixed
+      w-screen
+      h-screen
+      flex
+      justify-center
+      items-center
+      bg-gray-700 bg-opacity-70
+      top-0
+      left-0
+    "
+  >
+    <post-form :method="method" :id="id" @close="$emit('close')"/>
+  </div>
+</template>
+
+<script>
+import PostForm from '@/components/Forms/PostsForm.vue'
+export default {
+  name: 'PostModal',
+  components: {
+    PostForm
+  },
+  props: {
+    method: { type: String, required: true },
+    id: { type: Number }
+  }
+}
+</script>
