212ecf98197a948b7cf34585a0a5d8289eb609c3
Italo Canturil <italo.cantur@gmail.com>
2022-08-02 23:27:55 -0300

    feat: Add vue the mask

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Repository from '@/plugins/Repository'
+import VueTheMask from 'vue-the-mask'
 
 import '@/assets/styles/app.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Repository)
+Vue.use(VueTheMask)
 
 new Vue({
   router,
