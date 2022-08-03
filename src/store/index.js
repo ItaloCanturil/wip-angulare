f7e56dd90f27e967435fdf3ca64dfff5f2011a6d
Italo Canturil <italo.cantur@gmail.com>
2022-08-02 22:53:17 -0300

    feat: Create UserModule

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import posts from './modules/PostsModule'
+import users from './modules/UserModule'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    posts
+    posts,
+    users
   }
 })
---
+++ src/store/modules/UserModule.js
@@ -0,0 +1,47 @@
+const state = {
+  data: []
+}
+
+const actions = {
+  getAllUser ({ commit }) {
+    const data = localStorage.getItem('users')
+    if (data) commit('SET_DATA', JSON.parse(data))
+  },
+
+  create ({ commit }, payload) {
+    commit('SET_MORE', payload)
+  },
+
+  edit ({ commit }, payload) {
+    commit('UPDATE_DATA', payload)
+  },
+
+  delete ({ commit }, id) {
+    commit('DELETE_DATA', id)
+  }
+}
+
+const mutations = {
+  SET_DATA (state, data) {
+    state.data = data
+  },
+  UPDATE_DATA (state, data) {
+    state.data[data.id - 1] = data
+    window.localStorage.setItem('users', JSON.stringify(state.data))
+  },
+  SET_MORE (state, data) {
+    state.data.push(data)
+    window.localStorage.setItem('users', JSON.stringify(state.data))
+  },
+  DELETE_DATA (state, data) {
+    state.data.splice(data.userId - 1, 1)
+    window.localStorage.setItem('users', JSON.stringify(state.data))
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations
+}
